f5a563c5744c415e03a086adf833d1f1f6faf41c
Madray Haven <sgpublic2002@gmail.com>
2022-10-30 11:48:06 +0800

    add wolplus

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,8 +28,8 @@ git_clone() {
 
 # 添加软件源 fw876/helloworld
 git_clone https://github.com/fw876/helloworld.git package/helloworld
-# 为 RTL8111e 添加驱动 BROBIRD/openwrt-r816
-git_clone https://github.com/BROBIRD/openwrt-r8168.git package/BROBIRD/r8168
+# 添加软件源 sundaqiang/openwrt-packages
+git_clone https://github.com/sundaqiang/openwrt-packages.git package/sundaqiang
 
 if [ "$1" == "--local" ]; then
   # 本地拉取依赖
