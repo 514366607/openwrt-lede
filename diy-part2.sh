7d9ac92371409d748ff94e5bdd8b71196cb7b564
Madray Haven <sgpublic2002@gmail.com>
2023-02-14 16:06:15 +0800

    move ip to config

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,6 @@ fi
 sed -i 's/$(TOPDIR)\/staging_dir/\/tmp\/openwrt\/staging_dir/g' rules.mk
 sed -i 's/$(TOPDIR)\/build_dir/\/tmp\/openwrt\/build_dir/g' rules.mk
 
-# 修改默认 IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
